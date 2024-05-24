4c192d0be8be8d45d5d1a424738d9ec2c1f2646e
anjali jindal <sunshine@MacBook-Pro-109.local>
2024-05-24 12:36:22 -0700

    starting of databases

--- models.py
+++ models.py
@@ -16,7 +16,9 @@ def get_time():
 
 ### Define your table below
 #
-# db.define_table('thing', Field('name'))
+db.define_table('species', Field('common_name'))
+db.define_table('sightings', Field('sampling_event_identifier'),Field('common_name'), Field('observation_count'))
+db.define_table('checklists', Field('sampling_event_identifier'),Field('latitude'), Field('longitude'), Field('observation_date'), Field('time_observation'), Field('observer_id'), Field('duration_minute'))
 #
 ## always commit your models to avoid problems later
 
