5526a0527267332c0ca68fd3584653ec285185e2
Ivan Franco <ifrancoh@ucsc.edu>
2024-06-04 23:17:34 -0700

    moved to Leaflet but map not showing

--- static/js/index.js
+++ static/js/index.js
@@ -2,9 +2,29 @@
 
 // This will be the object that will contain the Vue attributes
 // and be used to initialize it.
-let app = {};
+//let app = {};
 
+//------- code from Leaflet ------------------
 
+let map;
+
+let app = {}
+
+app.init = () => {
+    app.map = L.map('map').setView([51.505, -0.09], 13)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright"OpenStreetMap</a>'
+    }).addTo(app.map);
+    // Adds listener
+    //app.map.on('click', app.click_listener);
+    //app.map.on('dbclick', app.dbclick_listener);
+};
+
+//----- end of Leaflet ----------------------
+
+// beginning of already contained code from starter_vue3
+/*let app = {};
 app.data = {    
     data: function() {
         return {
@@ -20,7 +40,7 @@ app.data = {
         },
     }
 };
-
+*/
 app.vue = Vue.createApp(app.data).mount("#app");
 
 app.load_data = function () {
