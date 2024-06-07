596d6bb5f6c77c3b28836701a64f98c275d4e33c
Rakshya Sharma <rakshya@ucsc-guest-169-233-120-215.ucsc.edu>
2024-06-06 22:13:19 -0700

    model changes

--- apps/bird_watching/models.py
+++ apps/bird_watching/models.py
@@ -43,19 +43,19 @@ db.define_table(
     Field('bird_count', 'integer')
 )
 if db(db.species).isempty():
-    with open('apps/bird_watching/species.csv', 'r') as f:
+    with open('apps/bird_watching/data/species.csv', 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             db.species.insert(common_name=row[0])
 
 if db(db.sightings).isempty():
-    with open('apps/bird_watching/sightings.csv', 'r') as f:
+    with open('apps/bird_watching/data/sightings.csv', 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             db.sightings.insert(sampling_event_identifier=row[0], common_name=row[1], observation_count=row[2])
 
 if db(db.checklists).isempty():
-    with open('apps/bird_watching/checklists.csv', 'r') as f:
+    with open('apps/bird_watching/data/checklists.csv', 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             db.checklists.insert(sampling_event_identifier=row[0], latitude=row[1], longitude=row[2], observation_date=row[3], time_observation=row[4], observer_id=row[5], duration_minute=row[6])
