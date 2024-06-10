2d831370841c401214d22c25e40415ff2bb36126
Rakshya Sharma <rakshya@ucsc-guest-169-233-120-215.ucsc.edu>
2024-06-10 08:15:16 -0700

    added stats page

--- apps/bird_watching/static/js/stats.js
+++ apps/bird_watching/static/js/stats.js
@@ -1,22 +1,20 @@
 "use strict";
-let app = {};
 
-app.data = {
-    data: function() {
-        return {
-            stats: [],
-            details: [],
-            trends: [],
-            search_query: '',
-            search_results: [],
-            selected_species: '',
-        };
+let app = new Vue({
+    el: '#app',
+    data: {
+        stats: [],
+        details: [],
+        trends: [],
+        search_query: '',
+        search_results: [],
+        selected_species: '',
     },
     methods: {
-        getStat(order = "recent"){
+        getStat(order = "recent") {
             this.selected_species = "";
             d3.select("#my_dataviz").selectAll("*").remove();
-            axios.get(get_stats_url + "/" + order).then((res) => {
+            axios.get(`${window.get_stats_url}/${order}`).then((res) => {
                 this.stats = res.data.species_list;
                 console.log("STATS:", this.stats);
             }).catch(err => {
@@ -24,9 +22,9 @@ app.data = {
             });
         },
 
-        getSpeciesDetails(species_name){
+        getSpeciesDetails(species_name) {
             this.selected_species = species_name;
-            axios.get(get_species_details_url + "/" + species_name).then((res) => {
+            axios.get(`${window.get_species_details_url}/${species_name}`).then((res) => {
                 this.details = res.data.sightings_data;
                 console.log("DETAILS:", this.details);
                 getGraph(this.details);
@@ -35,9 +33,9 @@ app.data = {
             });
         },
 
-        getTrend(){
+        getTrend() {
             this.selected_species = "";
-            axios.get(get_trends_url).then((res) => {
+            axios.get(window.get_trends_url).then((res) => {
                 this.trends = res.data.trend_data;
                 console.log("Trend:", this.trends);
                 getGraph(this.trends);
@@ -46,36 +44,26 @@ app.data = {
             });
         },
 
-        searchSpecies(){
-            axios.get(search_species_url, {params: {query: this.search_query}}).then((res) => {
+        searchSpecies() {
+            axios.get(window.search_species_url, { params: { query: this.search_query } }).then((res) => {
                 this.search_results = res.data.species_list;
                 console.log("SEARCH RESULTS:", this.search_results);
             }).catch(err => {
                 console.log("Failed at searchSpecies", err);
             });
         },
+    },
+    created() {
+        this.getStat('recent');
     }
-};
-
-app.vue = Vue.createApp(app.data).mount("#app");
-
-app.load_data = function () {
-    axios.get(get_stats_url + "/recent").then((res) => {
-        app.vue.stats = res.data.species_list;
-        console.log("LOAD:", app.vue.stats);
-    }).catch(err => {
-        console.log("Failed at LOAD", err);
-    });
-}
-
-app.load_data();
+});
 
 const getGraph = (data) => {
     console.log("Graph Data:", data);
 
     d3.select("#my_dataviz").selectAll("*").remove();
 
-    var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    var margin = { top: 30, right: 30, bottom: 70, left: 60 },
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
@@ -87,8 +75,8 @@ const getGraph = (data) => {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var x = d3.scaleBand()
-        .range([ 0, width ])
-        .domain(data.map(function(d) { return d.date; }))
+        .range([0, width])
+        .domain(data.map(function (d) { return d.date; }))
         .padding(0.2);
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
@@ -98,8 +86,8 @@ const getGraph = (data) => {
         .style("text-anchor", "end");
 
     var y = d3.scaleLinear()
-        .domain([0, Math.max(...data.map(function(d){return parseInt(d.count)})) + 1])
-        .range([ height, 0]);
+        .domain([0, Math.max(...data.map(function (d) { return parseInt(d.count); })) + 1])
+        .range([height, 0]);
     svg.append("g")
         .call(d3.axisLeft(y));
 
@@ -107,9 +95,9 @@ const getGraph = (data) => {
         .data(data)
         .enter()
         .append("rect")
-        .attr("x", function(d) { return x(d.date); })
-        .attr("y", function(d) { return y(parseInt(d.count)); })
+        .attr("x", function (d) { return x(d.date); })
+        .attr("y", function (d) { return y(parseInt(d.count)); })
         .attr("width", x.bandwidth())
-        .attr("height", function(d) { return height - y(parseInt(d.count)); })
+        .attr("height", function (d) { return height - y(parseInt(d.count)); })
         .attr("fill", "#69b3a2");
 }
